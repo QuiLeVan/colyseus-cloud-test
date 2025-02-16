3fb49d5b6e6144baa4dbd15da300ad284630a496
Le Van Qui <qui.levan@a5labs.co>
2025-02-16 07:12:24 +0700

    * test connect from client -> local server

--- client/assets/script/LobbyScene.ts
+++ client/assets/script/LobbyScene.ts
@@ -1,10 +1,31 @@
 import { _decorator, Component, Node } from 'cc';
+import Colyseus, { Room } from 'db://colyseus-sdk/colyseus.js';
 const { ccclass, property } = _decorator;
 
 @ccclass('LobbyScene')
 export class LobbyScene extends Component {
-    start() {
+    async start() {
+        const endpoint = 'ws://localhost:2567';
+        const roomName = "my_room";
+        
+        const client = new Colyseus.Client(endpoint);
+        const room: Room = await client.joinOrCreate(roomName, {
+          // your join options here...
+        });
 
+        console.log("joined successfully!");
+
+        room.onMessage("message-type", (payload) => {
+          // logic
+        });
+
+        room.onStateChange((state) => {
+          console.log("state change:", state);
+        });
+
+        room.onLeave((code) => {
+          console.log("left");
+        });
     }
 
     update(deltaTime: number) {
